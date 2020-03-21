bd7b6d6f47eed6267b5d71de33dfb0330dab7adb
AhmedAli8161 <59251840+AhmedAli8161@users.noreply.github.com>
2020-03-21 22:26:17 +0500

    Updates

---
+++ precache-manifest.bcaed3b68714996f1074ef0bca3c667a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a319941a2ca68f6806f819a8953b2934",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "83ebca96db23e0410db0",
+    "url": "/monsters-rolodex/static/css/main.042f12a9.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83ebca96db23e0410db0",
+    "url": "/monsters-rolodex/static/js/main.fa42b652.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
